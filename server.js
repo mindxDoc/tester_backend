e3c8b2e372cbb00fe1660cd87c43638930ee558b
mindxDoc <doc@mindx.edu.vn>
2023-06-21 16:01:47 +0700

    ready to deploy into vercel

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const db = require("./db");
-
+const path = require('path');
 const morgan = require('morgan');
 
 const app = express(),
